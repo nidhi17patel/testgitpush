dd1135fd9b02f821b32eed312b80b08263c8cc74
Nidhi Patel <nidhi17patel@gmail.com>
2022-07-17 19:26:39 +0530

    Hello, First Git Class

---
+++ mongotest.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Nidhi:1234@cluster0.whtng.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name" : "Nidhi",
+    "Email" : "nidhi17patel@gmail.com",
+    "surname" : "Patel"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
